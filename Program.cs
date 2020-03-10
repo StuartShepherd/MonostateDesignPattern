495d4c359afa7f7cb5625295c209ebfba8d46410
Stuart Shepherd <stuartshepherd@gmail.com>
2020-03-11 08:26:28 +1100

    updated title

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace Monostate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("monostate example");
-            Console.WriteLine("monostate is a creational design pattern that lets you ensure that a class has only one instance.");
-            Console.WriteLine("monostate pattern attempts to hide the fact that a class is only operating on a single instance of data by using static data members.");
+            Console.WriteLine("Monostate design pattern example");
+            Console.WriteLine("Monostate is a creational design pattern that lets you ensure that a class has only one instance.");
+            Console.WriteLine("Monostate pattern attempts to hide the fact that a class is only operating on a single instance of data by using static data members.");
 
             var person1 = new Monostate.Monostate
             {
